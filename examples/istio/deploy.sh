920335c1e5e01c4f9490d9ab707e786f33aac3b4
qinkeith <69738474+qinkeith@users.noreply.github.com>
2021-12-10 08:33:15 -0500

    Update deploy.sh

--- examples/istio/deploy.sh
+++ examples/istio/deploy.sh
@@ -2,17 +2,18 @@
 
 kind create cluster || echo cluster already exists
 istioctl manifest apply -y
-istioctl install --set profile=default --set revision=1-6-7
 
 kubectl create -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes-examples/keycloak.yaml
 kubectl rollout status deployment/keycloak
+
 source setup-keycloak.sh
 
 kubectl label namespace default istio-injection=enabled || true
+
 kubectl apply -f httpbin.yaml
 kubectl apply -f httpbin-gateway.yaml
-
 kubectl rollout status deployment/httpbin
+
 HTTPBIN_POD=$(kubectl get pods -lapp=httpbin -o jsonpath='{.items[0].metadata.name}')
 kubectl cp ../oidc.wasm  ${HTTPBIN_POD}:/var/local/lib/wasm-filters/oidc.wasm --container istio-proxy
 
